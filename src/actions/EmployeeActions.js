f93384a2fa685ddfb8aea0b548cb4fa8b4a1bc1d
may <wxm112@me.com>
2017-10-12 14:34:45 +1100

    clean out the form when create employee

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,11 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEE_FETCH_SUCCESS } from './types';
+import {
+    EMPLOYEE_UPDATE,
+    EMPLOYEE_CREATE,
+    EMPLOYEE_FETCH_SUCCESS,
+    EMPLOYEE_SAVE_SUCCESS
+} from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
     return {
@@ -36,11 +41,12 @@ export const employeesFetch = () => {
 export const employeeSave = ({ name, phone, shift, uid }) => {
     const { currentUser } = firebase.auth();
 
-    return () => {
+    return (dispatch) => {
         console.log(name, phone, shift, uid);
         firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
             .set({ name, phone, shift })
             .then(() => {
+                dispatch({ type: EMPLOYEE_SAVE_SUCCESS });
                 Actions.pop();
             });
     };
--- src/reducters/EmployeeFormReducer.js
+++ src/reducters/EmployeeFormReducer.js
@@ -1,4 +1,4 @@
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from '../actions/types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEE_SAVE_SUCCESS } from '../actions/types';
 
 const INITIAL_STATE = {
     name: '',
@@ -12,7 +12,9 @@ export default (state = INITIAL_STATE, action) => {
             return { ...state, [action.payload.prop]: action.payload.value };
         case EMPLOYEE_CREATE:
             return INITIAL_STATE;
+        case EMPLOYEE_SAVE_SUCCESS:
+            return INITIAL_STATE;
         default:
-        return state;
+            return state;
     }
 };
